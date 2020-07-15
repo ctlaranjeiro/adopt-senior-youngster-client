6fd7a71228fcaaf8f4b48c38f4a1a63381bfb81f
Bruno <bpais@outlook.pt>
2020-07-15 22:42:38 +0100

    assigned voluunteer page (can't access the assignedVolunteer full object)

--- src/components/user/AssignedVolunteers.js
+++ src/components/user/AssignedVolunteers.js
@@ -2,41 +2,71 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class AssignedVolunteers extends Component {
-    state = {
-        listOfVolunteers: []
-    }
+    // state = {
+    //     listOfVolunteers: this.props.loggedInAccount.assignedVolunteers
+    // }
 
-    getAllVolunteers = () => {
-        // Get list of volunteers from the API
-        axios.get(`${process.env.REACT_APP_SERVER}/api/user/:id/assignedVolunteers`)
-            .then(responseFromAPI => {
-                this.setState({
-                    listOfVolunteers: responseFromAPI.data
-                })
-            });
-    }
     
-    componentDidMount() {
-      this.getAllVolunteers();
-    }
+
+    // getAllVolunteers = () => {
+    //     Get list of volunteers from the API
+    //     axios.get(`${process.env.REACT_APP_SERVER}/api/user/:id`)
+    //         .then(responseFromAPI => {
+    //             this.setState({
+    //                 listOfVolunteers: this.props.loggedInAccount.assignedVolunteers
+    //             })
+    //         });
+    // }
+    
+    // componentDidMount() {
+    //   this.getAllVolunteers();
+    // }
 
     render(){
+        console.log('listOfVolunteers: ', this.props.loggedInAccount);
         return(
-            <div>
-                <h1>{this.props.loggedInAccount.firstName}'s Assigned Volunteers Page</h1>
+            <div fluid="md" className="">
+                <h1>{this.props.loggedInAccount && this.props.loggedInAccount.firstName}'s Assigned Volunteers Page</h1>
+                <p>
+                    Page under construction!
+                </p>
                 <div>
-                    {this.state.listOfVolunteers.map(vol => {
-                        return (
-                            <div>
-                                <h3>{vol.firstName} {vol.lastName}</h3>
-                                <div>
-                                    {vol}
-                                </div>
-                            </div>
-                        )
-                    })}
+                <h3>Assigned Volunteers</h3>
+                    <ul>
+                        {this.props.loggedInAccount && this.props.loggedInAccount.assignedVolunteers.map(vol => {
+                            return (
+                                <li>
+                                    <div>
+                                        <span>
+                                            <strong>
+                                                {/* {vol.firstName} {vol.lastName} */}
+                                                vol.firstName vol.lastName
+                                            </strong>
+                                        </span>
+                                        <span>
+                                            {/* {vol.profilePicture} */}
+                                            vol.profilePicture
+                                        </span>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
                 </div>
             </div>
         )
     }
 }
+/* 
+<Container>
+  <Row>
+    <Col>1 of 2</Col>
+    <Col>2 of 2</Col>
+  </Row>
+  <Row>
+    <Col>1 of 3</Col>
+    <Col>2 of 3</Col>
+    <Col>3 of 3</Col>
+  </Row>
+</Container>
+ */
